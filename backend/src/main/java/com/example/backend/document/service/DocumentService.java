4ffe3afdf950ca75703a668a7c02d32ed797feeb
root <ester.ovrani@gmail.com>
2025-10-19 02:58:01 +0300

    update document service file

--- backend/src/main/java/com/example/backend/document/service/DocumentService.java
+++ backend/src/main/java/com/example/backend/document/service/DocumentService.java
@@ -15,7 +15,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.text.PDFTextStripper;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
