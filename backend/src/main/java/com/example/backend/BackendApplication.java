603d32497a47864a235fe7c1c4127f772ed4633e
EsterOvrani <Ester.Ovrani@gmail.com>
2025-11-17 11:20:44 +0200

    fix the date and time

--- backend/src/main/java/com/example/backend/BackendApplication.java
+++ backend/src/main/java/com/example/backend/BackendApplication.java
@@ -3,13 +3,20 @@ package com.example.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
+import java.util.TimeZone;
+import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
 @EnableAsync
 public class BackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
-	}
+    @PostConstruct
+    public void init() {
+        // ✅ הגדר את TimeZone לכל האפליקציה
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Jerusalem"));
+    }
 
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
 }
